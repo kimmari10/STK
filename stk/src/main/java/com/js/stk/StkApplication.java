cc7b9d7a82d7fd5f304b3e9e2ca7aeb3cc2f2002
kimmari10 <kimmari1010@gmail.com>
2020-01-20 19:59:02 +0900

    initial commit

---
+++ stk/src/main/java/com/js/stk/StkApplication.java
@@ -0,0 +1,13 @@
+package com.js.stk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StkApplication.class, args);
+	}
+
+}
