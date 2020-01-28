e37a45ba85fad2138f1c76b15340a2524c60ac48
kimmari10 <kimmari1010@gmail.com>
2020-01-28 20:46:27 +0900

    costomizing servletContainer - replace webServer tomcat to undertow - set random port and write test code

--- stk/src/main/java/com/js/stk/StkApplication.java
+++ stk/src/main/java/com/js/stk/StkApplication.java
@@ -1,28 +1,13 @@
 package com.js.stk;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import me.js.jsSpringBootStarter.Holoman;
 
 @SpringBootApplication
 public class StkApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(StkApplication.class, args);
-		SpringApplication application = new SpringApplication(StkApplication.class);
-		application.setWebApplicationType(WebApplicationType.NONE);
-		application.run(args);
-	}
-	
-	@Bean
-	public Holoman holoman() {
-		Holoman holoman = new Holoman();
-		holoman.setHowLong(10);
-		holoman.setName("jongc");
-		return holoman;
+		SpringApplication.run(StkApplication.class, args);
 	}
 
 }
