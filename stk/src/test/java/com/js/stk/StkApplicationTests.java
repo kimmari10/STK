0dc1bbdec9d39a5ae7dc8e42eb4771c9601bf84a
kimmari10 <kimmari10@naver.com>
2020-02-04 20:10:10 +0900

    @TestPropertySource를 활용한 오류 해결방법

--- stk/src/test/java/com/js/stk/StkApplicationTests.java
+++ stk/src/test/java/com/js/stk/StkApplicationTests.java
@@ -7,9 +7,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@TestPropertySource(properties = "js.age=11")
 @SpringBootTest
 class StkApplicationTests {
 
