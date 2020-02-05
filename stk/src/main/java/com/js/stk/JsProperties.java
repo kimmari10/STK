db0332050c0ddcaf946947f9b635e7e948334a00
kimmari10 <kimmari10@naver.com>
2020-02-05 19:28:18 +0900

    properties에 @Validated 활용법

--- stk/src/main/java/com/js/stk/JsProperties.java
+++ stk/src/main/java/com/js/stk/JsProperties.java
@@ -1,13 +1,21 @@
 package com.js.stk;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Component
 @ConfigurationProperties("js.k")
+@Validated
 public class JsProperties {
+	
+	@NotEmpty
 	private String name;
 	
+	@Max(100)
 	private int age;
 	
 	public String getName() {
