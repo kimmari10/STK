b2ba55494245a90c387c7d0ae5a9dc712411f904
kimmari10 <kimmari10@naver.com>
2020-02-05 19:14:23 +0900

    properties를 bean으로 등록하는 방법

--- stk/src/main/java/com/js/stk/SampleRunner.java
+++ stk/src/main/java/com/js/stk/SampleRunner.java
@@ -1,5 +1,6 @@
 package com.js.stk;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -8,17 +9,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleRunner implements ApplicationRunner{
 
-	@Value("${js.name}")
-	String name;
-	
-	@Value("${js.age}")
-	int age;
-	
+	@Autowired
+	JsProperties jsProperties;
 	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		System.out.println("js name : " + name);
-		System.out.println("js age : " + age);
+		System.out.println("js name : " + jsProperties.getName());
+		System.out.println("js age : " + jsProperties.getAge());
 	}
 
 }
--- stk/src/main/java/com/js/stk/StkApplication.java
+++ stk/src/main/java/com/js/stk/StkApplication.java
@@ -4,6 +4,7 @@ import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
