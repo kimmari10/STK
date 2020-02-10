90af0df673f0c75d7db086c2ef3f0022bd090eef
kimmari10 <kimmari10@naver.com>
2020-02-10 20:15:25 +0900

    WebMvc 테스트 코드

--- stk/src/main/java/com/js/stk/StkApplication.java
+++ stk/src/main/java/com/js/stk/StkApplication.java
@@ -1,41 +1,17 @@
 package com.js.stk;
 
-import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StkApplication {
-	
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello Spring";
-	}
-	
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-		return tomcat;
-	}
-
-	private Connector createStandardConnector() {
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-		connector.setPort(7070);
-		return connector;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(StkApplication.class);
-		app.setWebApplicationType(WebApplicationType.NONE);
+		app.setWebApplicationType(WebApplicationType.SERVLET);
 		app.run(args);
 	}
 
